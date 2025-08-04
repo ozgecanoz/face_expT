916418b69122bc2289061b56ee75a86c165c403e
Ozge Whiting <ozgecanozcanli@gmail.com>
2025-08-04 11:05:38 -0400

    freeze expression transformer and only train reconstruction

--- face_model/run_expression_and_reconstruction_training.py
+++ face_model/run_expression_and_reconstruction_training.py
@@ -43,7 +43,7 @@ def main():
             'learning_rate': 5e-5,
             'warmup_steps': 3000,  # Learning rate warmup steps
             'min_lr': 1e-6,  # Minimum learning rate after decay
-            'batch_size': 2,  # for L4 GPU train-gpu-co 
+            'batch_size': 1,  # for L4 GPU train-gpu-co 
             'num_epochs': 3,
             'save_every_step': 600,   # Save similarity plots and checkpoints every 300 steps
             'num_workers': 8,  # for L4 GPU (24 GB VRAM) train-gpu-co (it has 16 vCPUs), memory 64GB, 
